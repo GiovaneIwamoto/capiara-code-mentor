4eda84dec34edb456d88f8cdf05c463113e38e63
Giovane Iwamoto <giovaneiwamoto@gmail.com>
2025-03-06 01:31:29 -0400

    feat: api maritalk http error

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import logging
 import streamlit as st
 from langchain_community.chat_models import ChatMaritalk
+from langchain_community.chat_models.maritalk import MaritalkHTTPError
 from langchain_core.messages import HumanMessage
 from langchain.schema import ChatMessage
 from langchain.callbacks.base import BaseCallbackHandler
@@ -10,8 +11,8 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(m
 logger = logging.getLogger(__name__)
 
 # Configure Streamlit
-st.set_page_config(page_title="🦜 Capiara Code Mentor")
-st.title('🦜 Capiara Code Mentor')
+st.set_page_config(page_title="Capiara Algorithm Mentor", layout="wide")
+st.title(":material/network_intel_node: Capiara Algorithm Mentor")
 
 class StreamHandler(BaseCallbackHandler):
     """Handles real-time streaming of LLM-generated tokens in Streamlit."""
@@ -49,29 +50,38 @@ def process_user_input(prompt: str, api_key: str):
     st.chat_message("user").write(prompt)
 
     if not api_key:
-        st.info("Please add your Maritalk API key to continue.")
+        st.info("Please add your Maritalk API key to continue.", icon=":material/passkey:")
         st.stop()
 
-    with st.chat_message("assistant"):
-        stream_handler = StreamHandler(st.empty())
-        
+    try:
         llm = ChatMaritalk(
             model="sabia-3",
             api_key=api_key,
             max_tokens=1000,
             stream=True,
-            callbacks=[stream_handler],
+            callbacks=[],
         )
 
         messages = [HumanMessage(content=prompt)]
-        response = ""
+        # Test API key by fetching the first response
+        next(llm.stream(messages))
 
+    except MaritalkHTTPError as e:
+        logger.error(f"API Error: {e}")
+        st.error("Invalid Maritalk API key, please enter a valid one.", icon=":material/key_off:")
+        st.stop()
+
+    with st.chat_message("assistant"):
+        stream_handler = StreamHandler(st.empty())
+
+        llm.callbacks = [stream_handler]
+
+        response = ""
         for chunk in llm.stream(messages):
             response += chunk.content
 
         st.session_state.messages.append(ChatMessage(role="assistant", content=response))
         logger.info("Response generated successfully")
-
 
 # Main application logic
 if __name__ == "__main__":
